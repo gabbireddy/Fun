ca8c217b7b3123d2db1f0f848bc77b6a8d0e16ea
gabbireddy <73250521+gabbireddy@users.noreply.github.com>
2020-10-23 00:31:45 -0700

    Update tixtax.py

--- tixtax.py
+++ tixtax.py
@@ -1,3 +1,45 @@
+#The aim of this software program is to create an interactive Tic Tac Toe game between two players.
+
+#Algorithm to create an interactice Tic Tac Toe game:
+#		1) Create a Tic Tac Toe board.
+#		2) Give player_1 the choice to choose between X and O
+#		3) Assign the choosen symbol to player_1 and the left over symbol to player_2
+#
+#
+#		#Start While Loop
+#		4) Ask player_1 which index position does he want to mark his symbol. (Update list)
+#		5) Check for "Victory":
+#						if Victory == True:
+#								Declare Player 1 as winner and exit the loop
+#						else:
+#							Ask player_2 the index position and update list
+#							Check for "Victory"
+#							if Victory == True:
+#								Declare Player 2 as winner and exit the loop
+#							else:
+#								Go back to step 4
+#
+# 		#Victory:
+#			#  Create 8 win conditions
+#			#  Check if any 1 of the 8 conditions is True
+#			#  If True, then assign True to Victory
+
+
+
+#TO DO NEXT
+# Validate user input while assigning X or O
+# Validate user input while giving the index position
+# Create an instance for Draw
+# Ask the user if he wants to play again
+# Validate the index input such that a player cannot overwrite a symbol in a game
+# Validate user input for playing again! 
+
+print("RULES OF THE GAME:\n")
+print("This is a game played between two players\n \nOne player chooses X and the other one gets an O \n")
+print("You will be playing the game on a 3x3 board\n \nThe first player to get 3 X's or O's in a line wins the game\n")
+print("Each box is labelled from 1 to 9, press the number of the box to mark your symbol")
+print("\nThe first box in the first row is 1, the second box in first row is 2 and so on...\n")
+input("Okay, so let's start the game \n  \nPress ENTER to continue")
 def tixtax():
 
 	#BOARD
@@ -5,11 +47,11 @@ def tixtax():
 
 	def display_board():
 		print('\n'*100)
-		print(f"  {b[7]} | {b[8]} | {b[9]}")
+		print(f"  {b[1]} | {b[2]} | {b[3]}")
 		print("  ----------")
 		print(f"  {b[4]} | {b[5]} | {b[6]}")
 		print("  ----------")
-		print(f"  {b[1]} | {b[2]} | {b[3]}")
+		print(f"  {b[7]} | {b[8]} | {b[9]}")
 
 #ASSIGN X or O to player 1
 	players = {"player_1":"temp","player_2":"temp"}
@@ -23,7 +65,7 @@ def tixtax():
 		while holder == False: #For validating the user input
 
 
-			players["player_1"] = input("Player 1, choose a symbol between X and O \n") #Assigning player_1 symbol
+			players["player_1"] = input("Player 1, choose a symbol between X and O \n").upper() #Assigning player_1 symbol
 
 			if players["player_1"] in ('X','O'):
 				holder = True
